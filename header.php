f853c74a1f4f374dcb585f66e853776c84e31b3e
AurelijusK <aurelijus.k@gmail.com>
2018-10-04 15:36:16 +0300

    2018-10-04

--- header.php
+++ header.php
@@ -6,6 +6,7 @@ require_once "functions.php";
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="stylenew.css">
     <title>
         <?php
