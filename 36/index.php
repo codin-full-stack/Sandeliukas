267dfc12ec379df010679c8236e53f31ea4395f8
AurelijusK <aurelijus.k@gmail.com>
2018-10-18 15:18:39 +0300

    2018-10-18

---
+++ 36/index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+</head>
+<body>
+    <div class='panel'>
+        <div class='panel-up'>
+            <div class='panel-flag'>
+                <button class='button red'>
+                <button class='button yellow'>
+                <button class='button green'>
+            
+            </div>
+            <div class='panel-input'>
+                <input class='input' type='number' value='0'>
+            </div>
+        </div>
+
+        <div class='panel-down'>
+            <button class='button-num symb'>A/C</button>
+            <button class='button-num symb'>+/-</button>
+            <button class='button-num symb'>%</button>
+            <button class='button-num symb'>/</button>
+            <button class='button-num'>7</button>
+            <button class='button-num'>8</button>
+            <button class='button-num'>9</button>
+            <button class='button-num symb'>x</button>
+            <button class='button-num'>4</button>
+            <button class='button-num'>5</button>
+            <button class='button-num'>6</button>
+            <button class='button-num symb'>-</button>
+            <button class='button-num'>1</button>
+            <button class='button-num'>2</button>
+            <button class='button-num'>3</button>
+            <button class='button-num symb'>+</button>
+            <button class='button-num long'>0</button>
+            <button class='button-num'>,</button>
+            <button class='button-num symb'>=</button>
+        </div>
+    </div>
+</body>
+
