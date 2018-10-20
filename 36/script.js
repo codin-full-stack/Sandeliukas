0a3c949d1e54b4896ecdd71a1520f90bf4df0626
AurelijusK <42798425+AurelijusK@users.noreply.github.com>
2018-10-20 21:20:28 +0300

    2018-10-20

--- 36/script.js
+++ 36/script.js
@@ -1,6 +1,34 @@
 var rez = document.querySelector(".input");
 var vals = [];
 
+
+
+
+function result2() {
+    if (rez.value.indexOf('%') > -1) {
+    
+        var numberString = rez.value.split(/\D+/);
+        var operatorString = rez.value.split(/[^\s()*/%+-]+/);
+        var opr=operatorString[1];
+        var num1=numberString[0];
+        var num2=numberString[0]/100*numberString[1];
+        rez.value=num1+opr+num2;
+        rez.value=eval(rez.value).toFixed(2);
+        var n = rez.value;
+        n = parseFloat(n);
+        rez.value=n;  
+    
+
+    }
+    else{
+    rez.value=eval(rez.value).toFixed(2);
+    var n = rez.value;
+    n = parseFloat(n);
+    rez.value=n;  
+    
+    }
+}
+
 function result(button) {
     var val = button.getAttribute('data-value');
 
@@ -20,7 +48,10 @@ function result(button) {
             var percents = (numbers[0] / 100) * parseInt(numbers[1]);
             var result = numbers[0] + symbol + percents;
 
-            rez.value=eval(result).toFixed(0);
+            rez.value=eval(result).toFixed(2);
+            var n = rez.value;
+            n = parseFloat(n);
+            rez.value=n;
             val='';
             vals = [];
             
