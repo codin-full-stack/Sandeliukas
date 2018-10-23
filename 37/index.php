2aea3f6aac4dabb771ef974d3bcf41216d229c32
AurelijusK <42798425+AurelijusK@users.noreply.github.com>
2018-10-23 22:34:33 +0300

    2018-10-23

--- 37/index.php
+++ 37/index.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
@@ -11,10 +12,52 @@
 </head>
 <body>
 
+
+<!--Navbar-->
+<nav class="navbar navbar-dark red lighten-1 mb-4">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#">Start Bootstrap</a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23"
+        aria-expanded="false" aria-label="Toggle navigation"><div class="animated-icon3"><span></span><span></span><span></span><span></span></div></button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent23">
+
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+        <!-- Links -->
+
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+
+
+
 <section  class='yellow'>
     <div class="container-fluid flex">
         <div class='title'>
-            <h1><B>Stylish Portfolio</B></h1>
+            <h1 class='helo'><B>Stylish Portfolio</B></h1>
             <h2><I>A Free Bootstrap Theme be Start Bootstrap</I></h2>
             <button class='button btn-blue'>Find Out More</button>
         </div>
@@ -79,10 +122,18 @@
             <h1><B>Recent Project</B></h1>
         </div>
         <div class='row'>                        
-            <div class="col-12 col-md-6 one portfolio-item"><a>STATIONARY</a></div>
-            <div class="col-12 col-md-6 two portfolio-item"><a>ICE CREAM</a></div>               
-            <div class="col-12 col-md-6 three portfolio-item"><a>STRAWBERIES</a></div>
-            <div class="col-12 col-md-6 four portfolio-item"><a>WORKSPACE</a></div>
+            <div class="col-12 col-md-6 one portfolio-item">
+                <a class="text-link">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus non eros auctor fringilla. Donec semper at nunc eu viverra. Quisque sollicitudin scelerisque orci in accumsan.</a>
+                <a class="port-link">STATIONARY</a></div>
+            <div class="col-12 col-md-6 two portfolio-item">
+                <a class="text-link">Nam nisi metus, vulputate a tellus sit amet, laoreet scelerisque tellus.</a>
+                <a class="port-link">ICE CREAM</a></div>              
+            <div class="col-12 col-md-6 three portfolio-item">
+                <a class="text-link">Suspendisse convallis vel risus et egestas. Duis aliquet massa finibus justo rutrum finibus.</a>
+                <a class="port-link">STRAWBERIES</a></div>
+            <div class="col-12 col-md-6 four portfolio-item">
+                <a class="text-link">In porta ante erat. Maecenas non placerat justo. Vestibulum lacus ex, sagittis aliquam massa in, dignissim fringilla ligula.</a>
+                <a class="port-link">WORKSPACE</a></div>
         </div>
     </div>
 </section>
@@ -110,9 +161,11 @@
     </div>
 </section>
 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 
-
-    <script type="text/javascript" src="script.js"></script>
+<!-- <script src="script.js"></script> -->
 </body>
 </html>
